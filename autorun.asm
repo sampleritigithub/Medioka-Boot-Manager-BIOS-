0596867cb967ef8ed2fed18270e74a6b4a97e2c4
Omar Ismail_Source <103457750+sampleritigithub@users.noreply.github.com>
2022-04-13 11:48:08 +0200

    Updated Comments
    Autorun (Boot Manager Main)
    ====================
    Version: 0.1.2

--- autorun.asm
+++ autorun.asm
@@ -11,9 +11,9 @@ ReadDrive:
     mov bx, programspace
     mov al, 4	 ; Disk Stuff you must do
     mov dl, [bootdisk]
-    mov ch, 0x00 ; Head of disk
-    mov dh, 0x00
-    mov cl, 0x02 ; Head of disk
+    mov ch, 0x00 ; Cylinder
+    mov dh, 0x00 ; Head
+    mov cl, 0x02 ; Sector
     int 0x13 ; BIOS Interrupt
     jc DiskReadFailed
     ret
